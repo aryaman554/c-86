41ba93e793047be865e65588f17f42fedd22bf75
aryaman554 <95351160+aryaman554@users.noreply.github.com>
2022-05-29 14:47:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	fabric.image.fromURL('BirthdayImage.jpg', function(img){
+        block_image_object = img;
+
+        block_image_object.scaletowidth(700);
+        block_image_object.scaletoheight(510);
+        block_image_object.set({
+            top:0
+            left:0
+        });
+        canvas.add(block_image_object);
+    });
+    }
+	
+
+
+function playSound(){
+    x.play();
+    
+	
+}
